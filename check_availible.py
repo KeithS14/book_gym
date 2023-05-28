e77eef1833dd3cf6e3cdca6a50a9aecdce794730
KeithS14 <kscannell11@gmail.com>
2023-05-28 01:26:36 +0100

    started development

---
+++ check_availible.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+# Specify the path to the ChromeDriver executable
+#webdriver_path = "C:\Users\kscan\OneDrive\Desktop\Random Code\chromedriver_win32\chromedriver.exe"
+
+driver = webdriver.Chrome()
+
+##driver.maximize_window()  
+
+url = 'https://app.glofox.com/portal/#/branch/5b6dd1a5e90c2d1f403fccb6/classes-day-view?header=classes,courses'
+
+driver.get(url)
+
+  
+page_source = driver.page_source
+
+
+print(page_source)
+
+driver.quit()
